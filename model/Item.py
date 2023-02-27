00d20f28da762a6925815bb1040752aca8c4e532
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2023-02-27 16:19:59 +0100

    Create Item.py

---
+++ model/Item.py
@@ -0,0 +1,24 @@
+from typing import Optional
+from pydantic import BaseModel, validator
+
+
+class Item(BaseModel):
+    id: Optional[int]
+    nome: str 
+    qta: int
+    
+    def setId(self, id):
+        self. id  = id
+    
+    def setNome(self, nome):
+        self. nome  = nome
+    
+    def setQta(self, qta):
+        self. qta  = qta
+
+    def __str__(self) -> str:
+        return f""" id:{self.id },
+                    nome:"{self.nome }", 
+                    qta:{self.qta }
+                """
+    
